b56ffbeda78b4f9577a2a6c08f16b70f1af9151f
Shlok Desai <shlokpdesai@gmail.com>
2020-08-08 18:10:25 -0700

    trial: db connection

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,12 @@
+import os
+import pymongo
 from flask import Flask
 
 application = Flask(__name__)
+
+def get_collection():
+    conn = pymongo.Connection(os.environ('OPENSHIFT_NOSQL_DB_URL'))
+    return conn.sampledb.samplecollection
 
 
 @application.route("/")
@@ -8,6 +14,21 @@ def home():
     status = dict()
     status["code"] = "This is great"
     return status
+
+
+@application.route("/post")
+def post():
+    collection = get_collection()
+    data = { "message": "Om Namah Shivay"}
+    inserted = collection.insert_one(data)
+    return { id: inserted.inserted_id }
+
+
+@application.route("/get-message")
+def get_message():
+    collection = get_collection()
+    data = collection.find_one()
+    return data
 
 
 if __name__ == "__main__":
